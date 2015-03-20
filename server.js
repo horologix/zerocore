65cd97986d197dfd95e78e89c9b7dcf439e6983b
lin <lin@zerocore.info>
2015-03-20 08:50:19 +0000

    .

--- server.js
+++ server.js
@@ -55,9 +55,12 @@ var Application = function() {
         };
 
         self.routes['/'] = function(req, res) {
-						res.redirect("http://google.com");
-						//res.setHeader('Content-Type', 'text/html');
-						//res.send(self.cache_get('index.html') );
+						res.setHeader('Content-Type', 'text/html');
+						res.send(self.cache_get('index.html') );
+        };
+
+        self.routes['/tx/:tx_id'] = function(req, res) {
+            res.send("you're attempting to access... "+res.params.tx_id);
         };
     };
 
